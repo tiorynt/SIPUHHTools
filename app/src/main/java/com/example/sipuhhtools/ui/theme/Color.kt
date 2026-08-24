735135f6a3d5bc0f9f14751fa5e0b65dc30d45d4
tiorynt <75600045+tiorynt@users.noreply.github.com>
2026-08-24 13:14:34 +0700

    style: add SIPUHH brown theme palette

--- app/src/main/java/com/example/sipuhhtools/ui/theme/Color.kt
+++ app/src/main/java/com/example/sipuhhtools/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.example.sipuhhtools.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BrownPrimary = Color(0xFF6B3F1D)
+val BrownOnPrimary = Color(0xFFFFFFFF)
+val BrownPrimaryContainer = Color(0xFFF0D9C2)
+val BrownOnPrimaryContainer = Color(0xFF2B1708)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val CreamBackground = Color(0xFFFFF8F3)
+val CreamSurface = Color(0xFFFFF8F3)
+val DarkText = Color(0xFF251A14)
+val OutlineBrown = Color(0xFF8D7565)
+
+val BrownDarkPrimary = Color(0xFFE4B98F)
+val BrownDarkOnPrimary = Color(0xFF422A17)
+val BrownDarkPrimaryContainer = Color(0xFF5B3A21)
+val BrownDarkOnPrimaryContainer = Color(0xFFFFDDBF)
+val DarkBackground = Color(0xFF1A120D)
+val DarkSurface = Color(0xFF1A120D)
